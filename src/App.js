a2d094d53ea056d96c14716a47a8f2b165faaca2
ajivesh <jivesh.arya@gmail.com>
2023-06-10 16:45:15 +0530

    made portfolio

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import indexRoutes from "./routes/index";
+
+function App() {
+  //   const [loading, setLoading] = useState(false);
+
+  //   useEffect(() => {
+  //     setLoading(true);
+  //     setTimeout(() => {
+  //       setLoading(false);
+  //     }, 5000);
+  //   }, []);
+  return (
+    <>
+      <Router>
+        <Routes>
+          {indexRoutes.map((prop, key) => {
+            return (
+              <Route
+                path={prop.path}
+                key={key}
+                element={prop.component}
+              ></Route>
+            );
+          })}
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/projectData.js
@@ -0,0 +1,24 @@
+export const ProjectData = [
+  {
+    title: "Tindog",
+    description:
+      "An interactive UI for a conceptualized dating application for dogs, built to responsive on all devices.",
+    img: "https://ik.imagekit.io/jivesharya/Screenshot_2023-06-10_154012.png?updatedAt=1686391847803",
+    stack: "Bootstrap",
+    link: "https://ajivesh.github.io/TinDog/",
+  },
+  {
+    title: "Drum Kit",
+    description: "A simple drum kit built with HTML, CSS and JavaScript.",
+    img: "https://ik.imagekit.io/jivesharya/Screenshot_2023-06-10_153600.png?updatedAt=1686391760729",
+    stack: "javascript",
+    link: "https://ajivesh.github.io/Drum-Kit/",
+  },
+  {
+    title: "Dice",
+    description: "A simple dice game built with HTML, CSS and JavaScript.",
+    img: "https://ik.imagekit.io/jivesharya/Screenshot_2023-06-10_153511.png?updatedAt=1686391760485",
+    stack: "javascript",
+    link: "https://ajivesh.github.io/dicee/",
+  },
+];
